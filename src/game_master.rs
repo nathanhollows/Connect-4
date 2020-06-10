ab38405e91d04578735846134c3cbe32acd94618
markuz449 <markuz449@gmail.com>
2020-06-10 13:13:11 +1200

    Changed all i8 types to usize

--- src/game_master.rs
+++ src/game_master.rs
@@ -1,15 +1,11 @@
 use crate::player;
-use::std::convert::TryInto;
-
-const X: usize = 7;
-const Y: usize = 6;
 
 // Starts the game of Connect 4
 pub fn start_game(){
-    let mut board = [[0; Y]; X];
+    let mut board = vec![vec![0; 6]; 7];
     let coin_flip = 1;
-    let player1: i8 = 1;
-    let player2: i8 = 2;
+    let player1: usize = 1;
+    let player2: usize = 2;
     
     println!("Game Start!");
     if coin_flip == 1{
@@ -21,17 +17,13 @@ pub fn start_game(){
 }
 
 // Takes in a move made by a player and updates the board
-pub fn update_board(update: &mut [[i32; Y]; X], posX: i8, posY: i8, player_num: i8) -> &mut [[i32; Y]; X]{
-    //board_print[0][5] = 1;
-    let _posX: usize = posX.try_into().unwrap();
-    let _posY: usize = posY.try_into().unwrap();
-    
-    update[_posX][_posY] = player_num.into();
+pub fn update_board(update: &mut Vec<Vec<usize>>, posX: usize, posY: usize, player_num: usize) -> &mut Vec<Vec<usize>>{
+    update[posX][posY] = player_num;
     update
 }
 
 // Checks the players turn, If the game is won, and continues the game loop
-pub fn game_loop(board: &mut [[i32; Y]; X], mut player_num: i8){
+pub fn game_loop(board: &mut Vec<Vec<usize>>, mut player_num: usize){
     if board_check(board){
         println!("Player {} Won the game!", player_num);
         std::process::exit(0);
@@ -47,17 +39,17 @@ pub fn game_loop(board: &mut [[i32; Y]; X], mut player_num: i8){
 }
 
 // Checks the board to see if the game has been won
-fn board_check(board: &[[i32; Y]; X]) -> bool{
+fn board_check(board: &Vec<Vec<usize>>) -> bool{
     let game_won: bool = false;
     
     game_won
 }
 
 // Prints out the current board of Connect 4
-pub fn print_board(board_print: & [[i32; Y]; X]){
+pub fn print_board(board_print: &Vec<Vec<usize>>){
     println!("Current Board:");
-    for y_print in 0..Y{
-        for x_print in 0..X{
+    for y_print in 0..6{
+        for x_print in 0..7{
             if board_print[x_print][y_print] == 1{
                 print!("Y ");
             } else if board_print[x_print][y_print] == 2{
--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,8 @@
 use crate::game_master;
 use std::io;
-use::std::convert::TryInto;
-
-const X: usize = 7;
-const Y: usize = 6;
 
 // Takes input from the players turn
-pub fn player_turn(board: &mut [[i32; Y]; X], player_num: i8){
+pub fn player_turn(board: &mut Vec<Vec<usize>>, player_num: usize){
     game_master::print_board(& board);
     println!("-------------");
     println!("1 2 3 4 5 6 7");
@@ -16,32 +12,35 @@ pub fn player_turn(board: &mut [[i32; Y]; X], player_num: i8){
     io::stdin().read_line(&mut input_text).expect("Failed to read from stdin");
 
     let trimmed = input_text.trim();
-    match trimmed.parse::<i8>() {
+    match trimmed.parse::<usize>() {
         Ok(choice_num) => enter_choice(choice_num, board, player_num),
         Err(..) => error_choice(board, player_num),
     };
 }
 
 // Checks if the choice is valid and if so, updates the board
-pub fn enter_choice(mut choice_num: i8, mut board: &mut [[i32; Y]; X], player_num: i8){
-    choice_num -= 1;
-    let choice_check: usize = choice_num.try_into().unwrap();
-    if (choice_num > -1 && choice_num < 6) && (board[choice_check][0] == 0){
+pub fn enter_choice(mut choice_num: usize, mut board: &mut Vec<Vec<usize>>, player_num: usize){
+    if choice_num == 0{
+        error_choice(board, player_num);
+    } else{
+        choice_num -= 1;
+    }
+    if choice_num < 7 && board[choice_num][0] == 0{
         for placement in (0..6).rev(){
-            let placement_check: usize = placement.try_into().unwrap();
-            if board[choice_check][placement_check] == 0{
-                //board = game_master::update_board(board, choice_num, placement, player_num);
+            if board[choice_num][placement] == 0{
+                board = game_master::update_board(board, choice_num, placement, player_num);
                 break;
             }
         }
-    //game_master::game_loop(board, player_num);
+        game_master::game_loop(board, player_num);
     } else{
-        //error_choice(board, player_num);
+        
+        error_choice(board, player_num);
     }
 }
 
 // Error code if the user enetered a value that is not valid
-fn error_choice(board: &mut [[i32; Y]; X], player_num: i8){
+fn error_choice(board: &mut Vec<Vec<usize>>, player_num: usize){
     println!("This is not a valid option");
     println!("Please enter a valid option");
     player_turn(board, player_num);
@@ -50,17 +49,14 @@ fn error_choice(board: &mut [[i32; Y]; X], player_num: i8){
 #[cfg(test)]
 mod tests{
     use crate::player;
-    const X: usize = 7;
-    const Y: usize = 6;
     
     #[test]
     fn test_valid(){
-        player::enter_choice(7, &mut [[0; Y]; X], 1);
+        player::enter_choice(7, &mut vec![vec![0; 6]; 7], 1);
     }
     
     #[test]
-    #[should_panic]
     fn test_wrong(){
-        player::enter_choice(0, &mut [[0; Y]; X], 1);
+        player::enter_choice(8, &mut vec![vec![0; 6]; 7], 1);
     }
 }
